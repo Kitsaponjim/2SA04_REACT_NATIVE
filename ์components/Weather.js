48723d94cbffcd32a487b8218f511b557d53210c
Kitsaponjim <98834570+Kitsaponjim@users.noreply.github.com>
2022-08-27 15:48:06 +0700

    Part4-Connect

--- ์components/Weather.js
+++ ์components/Weather.js
@@ -1,12 +1,31 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import { ImageBackground, Text , StyleSheet, View} from "react-native";
 import Forecast from "./Forecast";
 import Constants from 'expo-constants'
 
 export default function Weather(props) {
+    useEffect(() => {
+        console.log(`fetching data with zipCode = ${props.zipCode}`);
+        if (props.zipCode) {
+          fetch(
+            `http://api.openweathermap.org/data/2.5/weather?q=90110,th&units=metric&APPID=57c578dc612a8dcd9512aca71c107a6c`
+          )
+            .then((response) => response.json())
+            .then((json) => {
+              setForecastInfo({
+                main: json.weather[0].main,
+                description: json.weather[0].description,
+                temp: json.main.temp,
+              });
+            })
+            .catch((error) => {
+              console.warn(error);
+            });
+        }
+      }, [props.zipCode]);
     const [forecastInfo, setForecastInfo] = useState({
-        main: '',
-        description: '',
+        main: '-',
+        description: '-',
         temp: 0
     })
     return (
