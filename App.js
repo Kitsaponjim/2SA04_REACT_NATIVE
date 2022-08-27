71140863f6c625860bee7b5220719eeecaa7013e
Kitsaponjim <98834570+Kitsaponjim@users.noreply.github.com>
2022-08-27 09:19:57 +0700

    Part1-Source Code

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const doIt = () => {
+    console.log("Hello world")
+  }
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text onPress={doIt}>Hello world</Text>
       <StatusBar style="auto" />
     </View>
   );
